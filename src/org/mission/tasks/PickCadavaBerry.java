faee3137b1a4dfc14e5f43a5d95608f8c91aab74
Dad <Dad>
2017-01-19 14:52:23 -0500

    Fixed cadava berry picking...

--- src/org/mission/tasks/PickCadavaBerry.java
+++ src/org/mission/tasks/PickCadavaBerry.java
@@ -2,8 +2,9 @@ package org.mission.tasks;
 
 import org.mission.OrionRJ;
 import org.mission.data.enums.QuestObject;
-import org.osbot.rs07.api.model.Item;
+import org.osbot.rs07.api.map.Area;
 import org.osbot.rs07.api.model.RS2Object;
+
 import viking.api.Timing;
 import viking.framework.task.Task;
 
@@ -11,10 +12,7 @@ import viking.framework.task.Task;
  * Created by Sphiinx on 1/10/2017.
  */
 public class PickCadavaBerry extends Task<OrionRJ> {
-
-    private RS2Object cadava_bush;
-    private RS2Object empty_cadava_bush;
-
+	
     public PickCadavaBerry(OrionRJ mission) {
         super(mission);
     }
@@ -26,25 +24,16 @@ public class PickCadavaBerry extends Task<OrionRJ> {
 
     @Override
     public void execute() {
-        cadava_bush = objects.closest(QuestObject.CADAVA_BERRY.getObjectArea(), QuestObject.CADAVA_BERRY.getObjectIDs());
-        if (cadava_bush != null && map.canReach(cadava_bush)) {
-            if (myPlayer().isMoving() || myPlayer().getAnimation() != -1)
-                return;
-
-            final Item[] inventory_cache = inventory.getItems();
-            if (cadava_bush.interact(QuestObject.CADAVA_BERRY.getAction()))
-                Timing.waitCondition(() -> inventory.getItems().length != inventory_cache.length || myPlayer().isMoving(), 150, random(2000, 2500));
-        } else {
-            empty_cadava_bush = objects.closest(QuestObject.CADAVA_BERRY.getObjectArea(), 23627);
-            if (empty_cadava_bush == null) {
-                if (walkUtils.walkToArea(QuestObject.CADAVA_BERRY.getObjectArea(), () -> {
-                    cadava_bush = objects.closest(QuestObject.CADAVA_BERRY.getObjectArea(), QuestObject.CADAVA_BERRY.getObjectIDs());
-                    return cadava_bush != null && cadava_bush.isVisible() && map.canReach(cadava_bush);
-                })) {
-                    Timing.waitCondition(() -> objects.closest(QuestObject.CADAVA_BERRY.getObjectArea(), QuestObject.CADAVA_BERRY.getObjectIDs()) != null, 150, random(2000, 2500));
-                }
-            }
-        }
+    	final Area AREA = QuestObject.CADAVA_BERRY.getObjectArea();
+    	if(AREA.contains(myPosition()))
+    		walkUtils.walkToArea(AREA);
+    	else
+    	{
+    		RS2Object bush = objects.closest(QuestObject.CADAVA_BERRY.getObjectIDs());
+    		if(bush != null && iFact.clickObject("Pick-from", bush).execute())
+    			Timing.waitCondition(() -> inventory.contains(QuestObject.CADAVA_BERRY.getItemID()), 5000);
+    	}
+ 
     }
 
     @Override
